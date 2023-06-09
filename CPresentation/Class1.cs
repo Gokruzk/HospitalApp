3217cfd4e9181ba9ff963300e4f645664978740b
gokrusk <nigelljama@gmail.com>
2023-06-08 21:49:11 -0500

    start project

---
+++ CPresentation/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPresentation
+{
+    public class Class1
+    {
+    }
+}
