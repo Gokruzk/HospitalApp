ea7d86e096248e47f6748e4094c0032bd6a13974
Fernando Novillo <70916955+Comtex765@users.noreply.github.com>
2023-06-10 23:11:16 -0500

    validación para estado vacaciones

---
+++ CLogic/ClOperacionesEstadoVacacion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLogic
+{
+    public class ClOperacionesEstadoVacacion
+    {
+        public bool ValidarEstado(int estado)
+        {
+            if (estado == 1 ||  estado == 2)
+                return true;
+
+            return false;
+        }
+    }
+}
