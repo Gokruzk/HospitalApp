164296e81c1a473b59bdad5a63eb0d877e661fff
Kevin <kevin@gmail>
2023-06-12 00:01:14 -0500

    Agrego el buscar empleado :’v

--- CapaPresentacion/FrmBuscarEmpleado.cs
+++ CapaPresentacion/FrmBuscarEmpleado.cs
@@ -7,11 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CLogic;
+using Entidades;
 
 namespace CapaPresentacion
 {
     public partial class FrmBuscarEmpleado : Form
     {
+        Empleado objEmpleado = new Empleado();
+        readonly ClOperacionesEmpleado objOperacionesEmpleado = new ClOperacionesEmpleado();
+        readonly ClOperacionesPoblacion objOperacionPoblacion = new ClOperacionesPoblacion();
+
         public FrmBuscarEmpleado()
         {
             InitializeComponent();
@@ -39,7 +45,7 @@ namespace CapaPresentacion
             {
                 try
                 {
-
+                    BtnBuscar.Focus();
                 }
                 catch (Exception ex)
                 {
@@ -53,12 +59,43 @@ namespace CapaPresentacion
         {
             try
             {
-                
+                if (CmbCampo.SelectedIndex == 0) //|| CmbCampo.SelectedItem.ToString() == "NUM.SEGURIDAD SOCIAL")
+                {
+                    objEmpleado = objOperacionesEmpleado.BuscarEmpleado(TxtEleccion.Text);
+
+                    if (objEmpleado != null) {
+                        asignar();
+                    } else {
+                        MessageBox.Show("No se encuentra a un empleado con la cédula ingresada en la base de datos", "INFO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                if (CmbCampo.SelectedIndex == 1) {
+                    objEmpleado = objOperacionesEmpleado.BuscarEmpleadoNumSocial(TxtEleccion.Text);
+
+                    if (objEmpleado != null) {
+                        asignar();
+                    } else {
+                        MessageBox.Show("No se encuentra a un empleado con el NSS ingresado en la base de datos", "INFO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
             }
             catch (Exception ex)
             {
                 MessageBox.Show($"Error BtnBuscar: " + ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+
+        private void asignar() {
+            lblNombre.Text = "NOMBRE:     " + objEmpleado.Nombre;
+            LblFechaNacimiento.Text = "FECHA DE NACIMIENTO:     " + objEmpleado.FechaNacimiento.ToShortDateString();
+            LblDireccion.Text = "DIRECCIÓN:     " + objEmpleado.Direccion;
+            LblPoblacion.Text = "POBLACIÓN:     " + objOperacionPoblacion.CargarPoblacion(objEmpleado.Poblacion - 1);
+            LblTipo.Text = "TIPO EMPLEADO:   " + objOperacionesEmpleado.CargarTiposEmpleado()[objEmpleado.Tipo - 1];
+            LblNumSegSocial.Text = "NÚMERO DE\nSEGURIDAD SOCIAL:    " + objEmpleado.NumSegSocial;
+
+            string[] estado = { "Habilitado", "No habilitado" };
+
+            LblEstado.Text = "ESTADO:     " + estado[objEmpleado.Estado - 1];
         }
     }
 }
