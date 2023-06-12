ad15d132a6c333c880239687389aa4b8760002eb
gokrusk <nigelljama@gmail.com>
2023-06-12 00:04:14 -0500

    reportes

--- CapaPresentacion/Reportes.Designer.cs
+++ CapaPresentacion/Reportes.Designer.cs
@@ -32,23 +32,27 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.lblBuscarPaciente = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.WhiteSmoke;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(162, 153);
+            this.dataGridView1.Location = new System.Drawing.Point(14, 152);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(466, 256);
+            this.dataGridView1.Size = new System.Drawing.Size(808, 277);
             this.dataGridView1.TabIndex = 0;
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.panel1.Controls.Add(this.lblBuscarPaciente);
-            this.panel1.Location = new System.Drawing.Point(14, 12);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(829, 78);
@@ -59,12 +63,12 @@
             this.lblBuscarPaciente.AutoSize = true;
             this.lblBuscarPaciente.Font = new System.Drawing.Font("Arial Rounded MT Bold", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblBuscarPaciente.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.lblBuscarPaciente.Location = new System.Drawing.Point(237, 23);
+            this.lblBuscarPaciente.Location = new System.Drawing.Point(333, 25);
             this.lblBuscarPaciente.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.lblBuscarPaciente.Name = "lblBuscarPaciente";
-            this.lblBuscarPaciente.Size = new System.Drawing.Size(276, 32);
+            this.lblBuscarPaciente.Size = new System.Drawing.Size(162, 32);
             this.lblBuscarPaciente.TabIndex = 1;
-            this.lblBuscarPaciente.Text = "BUSCAR PACIENTE";
+            this.lblBuscarPaciente.Text = "REPORTES";
             // 
             // comboBox1
             // 
@@ -72,17 +76,28 @@
             this.comboBox1.Items.AddRange(new object[] {
             "Medicos con tipos",
             "Medicos y sus vacaciones"});
-            this.comboBox1.Location = new System.Drawing.Point(223, 97);
+            this.comboBox1.Location = new System.Drawing.Point(317, 116);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 90;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(83, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(228, 18);
+            this.label1.TabIndex = 91;
+            this.label1.Text = "SELECCIONE UN REPORTE";
             // 
             // Reportes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(834, 441);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.dataGridView1);
@@ -94,6 +109,7 @@
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -103,5 +119,6 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label lblBuscarPaciente;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
